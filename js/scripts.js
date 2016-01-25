d6a2cae6ea0ccbf99dc806d5db3e8537563748e9
mary warrington and paul hess <pauljacksonhess@gmail.com>
2016-01-25 11:20:08 -0800

    add UI to successfully display inputs in a list

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,25 @@ function Place(placeName, placeCompanion, placeReturn) {
   this.placeCompanion = placeCompanion;
   this.placeReturn = placeReturn;
 }
+
+$(document).ready(function() {
+  $('form#placeForm').submit(function(event) {
+    event.preventDefault();
+    var placeName = $('#placeInput').val();
+    var placeCompanion = $('#companionInput').val();
+    var placeReturn = $('input:checked').val();
+
+    var newPlace = new Place(placeName, placeCompanion, placeReturn);
+    console.log(newPlace)
+
+    $('placeListHidden').show();
+    $('ul#placeList').append('<li class="placeItems">' + placeName + '</li>');
+
+    $('li.placeItems').last().click(function() {
+      $("div#resultsList").show();
+      $("span#placeOutput").text(newPlace.placeName);
+      $("span#companionOutput").text(newPlace.placeCompanion);
+      $("span#returnOutput").text(newPlace.placeReturn);
+    });
+  });
+});
